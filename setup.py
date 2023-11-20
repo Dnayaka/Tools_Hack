bf43ef9f927b1875b1babd09eda3ea4353d37bbd
Dnayaka <123858514+Dnayaka@users.noreply.github.com>
2023-11-20 21:14:56 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+os.system("pip3 install beautifulsoup4")
+os.system("pip3 install phonenumbers")
+os.system("pip3 install rich")
+os.system("pip3 install requests")
+os.system("pip3 install cloudscraper")
+os.system("pip3 install socks")
+os.system("pip3 install pysocks")
+os.system("pip3 install colorama")
+os.system("pip3 install undetected_chromedriver")
+os.system("pip3 install httpx")
+
+if os.name == "nt":
+    pass
+else:
+    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
+
+print("Done.")
