0ea76ca1089511956d154e6c0c9bd8f4a66a7145
Dnayaka <123858514+Dnayaka@users.noreply.github.com>
2023-11-22 17:33:35 +0700

    Hacking Tools Setup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import os
+
 os.system("pkg install git")
 os.system("git clone https://github.com/cutipu/HASOKI.git")
 os.system("git clone https://github.com/HunxByts/GhostTrack.git")
 os.system("git clone https://github.com/KasRoudra/MaxPhisher.git")
+os.system("git clone https://github.com/TheSpeedX/TBomb.git")
 os.system("git clone https://github.com/Dnayaka/CRACK_FB.git")
+os.system("pip3 install certifi")
+os.system("pip3 install chardet")
+os.system("pip3 install colorama")
+os.system("pip3 install idna")
+os.system("pip3 install requests")
 os.system("pip3 install beautifulsoup4")
 os.system("pip3 install phonenumbers")
 os.system("pip3 install rich")
@@ -15,10 +22,5 @@ os.system("pip3 install colorama")
 os.system("pip3 install undetected_chromedriver")
 os.system("pip3 install httpx")
 
-if os.name == "nt":
-    pass
-else:
-    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
 
 print("Done.")
